40f6fed087811fabcd8ce769892aa9d094d48e9a
OriBer12 <60361382+OriBer12@users.noreply.github.com>
2020-02-27 16:35:59 +0200

    bar graph

--- frontend/src/components/Graph/Graph.js
+++ frontend/src/components/Graph/Graph.js
@@ -2,26 +2,7 @@ import React, { Component } from 'react';
 import './Graph.css';
 import { Chart } from "react-google-charts";
 
-const data = [
-  ["Date", "Sales"],
-  ["1.2", 20],
-  ["2.2", 18],
-  ["3.2", 21],
-  ["4.2", 22],
-  ["5.2", 20],
-  ["6.2", 23],
-  ["7.2", 24],
-  ["8.2", 25],
-  ["9.2", 26],
-  ["10.2", 27],
-  ["11.2", 28],
-  ["12.2", 29],
-  ["13.2", 23],
-  ["14.2", 18],
-  ["15.2", 19],
-  ["16.2", 18]
 
-];
 class Graph extends Component {
   // constructor(props){
     // super(props);
@@ -41,16 +22,16 @@ class Graph extends Component {
     const options = {
       title: "",
       hAxis: { title: "Date", viewWindow: { min: 0, max: 15 } },
-      
+
       curveType: "function",
       legend: { position: "bottom" }
     };    return (
       <div className={"my-pretty-chart-container"}>
         <Chart
-          chartType="LineChart"
-          width="100%"
-          height="320px"
-          data={data}
+          chartType= {this.props.type}
+          width={this.props.width}
+          height={this.props.height}
+          data={this.props.data}
           options={options}
         />
       </div>
--- frontend/src/pages/TargetDashboard/TargetDashboard.js
+++ frontend/src/pages/TargetDashboard/TargetDashboard.js
@@ -6,12 +6,36 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card';
 
-import Target from '../../components/Target';
 import Carousel from '../../components/carousel';
 import Performance from '../../components/performance';
 import Panel from '../../components/panel';
 import Graph from '../../components/Graph';
 
+const line = [
+  ["Date", "Sales"],
+  ["1.2", 20],
+  ["2.2", 18],
+  ["3.2", 21],
+  ["4.2", 22],
+  ["5.2", 20],
+  ["6.2", 23],
+  ["7.2", 24],
+  ["8.2", 25],
+  ["9.2", 26],
+  ["10.2", 27],
+  ["11.2", 28],
+  ["12.2", 29],
+  ["13.2", 23],
+  ["14.2", 18],
+  ["15.2", 19],
+  ["16.2", 18]
+
+];
+const bar=[
+    ["Device","Me","Avg. Group"],
+    ['iPhone XR', 20, 30],
+    ['Samsung S20', 30, 28]
+  ];
 class TargetDashboard extends Component {
   // constructor(props){
     // super(props);
@@ -37,7 +61,7 @@ class TargetDashboard extends Component {
               <Card className="dashboard-card">
               <Card.Title className="dashboard-card-title">My Monthly Performance</Card.Title>
               <Container className="Container">
-                <Graph/>
+                <Graph type="LineChart" width="100%" height="320px" data={line}/>
               </Container>
               <Card.Footer className="dashboard-card-footer">
                 <p className="dashboard-card-footer-header">show more</p>
@@ -63,8 +87,8 @@ class TargetDashboard extends Component {
             <Card className="dashboard-card">
               <Card.Title className="dashboard-card-title">My Performance</Card.Title>
               <Container className="Container">
-                <Row className="row">
-                  <Performance/>
+                <Row className="row" >
+                  <Graph type="Bar" width="100%" height="320" data={bar}/>
                 </Row>
               </Container>
               <Card.Footer className="dashboard-card-footer">
