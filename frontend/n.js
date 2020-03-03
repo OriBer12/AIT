c93a72f8067a3db2693e090e11d89034af59ed35
OriBer12 <60361382+OriBer12@users.noreply.github.com>
2020-03-03 17:20:32 +0200

    header bold

--- frontend/n.js
+++ frontend/n.js
@@ -4,24 +4,25 @@ const readXlsxFile = require('read-excel-file/node');
 // File path.
 readXlsxFile('1.xlsx').then((rows) => {
 	temp = {};
+	temp["kpi"] = [];
   rows[0].forEach((element,index) => {
 	  console.log('--------' + element + '----------');
 	  if(rows[1][index] == null){
 	  	temp[element] = null;
 	  }else{
-
-		  if(temp[element] == undefined){
-
-	  		temp[element] = {};
-	  		ele = element;
-		  }else{
-
-			while(rows[1][index] != null){
-				console.log('--------' + rows[1][index] + '----------');
-				console.log(rows[1][index]);
-	  	  		temp[ele][rows[1][index]] = null;
-				rows[1].shift();
-	  		}
+			if(temp[element] == undefined){
+				temp[element] = {};
+				ele = element;
+	  	}else{
+				kpi = {};
+				kpi["name"] = element;
+				while(rows[1][index] != null){
+					console.log('--------' + rows[1][index] + '----------');
+					console.log(rows[1][index]);
+		  	  		temp[ele][rows[1][index]] = null;
+					rows[1].shift();
+				}
+				temp["kpi"].push(kpi);
 		}
 	  }
     rows[1].shift();
